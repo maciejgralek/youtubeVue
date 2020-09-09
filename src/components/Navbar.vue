3091969f43aa6c42a15ec83176a1ccdbb5569abd
ender <ender@localhost>
2020-09-09 21:20:03 +0200

    add navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
 	<!-- <nav class="navbar navbar&#45;expand&#45;lg navbar&#45;dark bg&#45;dark  "> -->
-	<nav class="navbar navbar-expand-lg shadow-sm">
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
     <a class="navbar-brand font-weight-bold" href="#">YTPlay</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,4 +20,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
+.navbar {
+	-webkit-box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.7);
+	-moz-box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.7);
+	box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.7);
+}
 </style>
