052371bb1197e7cb5951a10d895d2e3de022aa34
ender <ender@localhost>
2020-09-13 13:40:35 +0200

    add compact view in playlist

--- src/use-UI.js
+++ src/use-UI.js
@@ -1,15 +1,15 @@
 import { ref } from 'vue'
 
 let playerHeight = ref(null);
-let compactMode = ref(false);
-let thumbnailWidth = ref(80);
-let thumbnailHeight = ref(50);
+let compactMode = ref(true);
 let _compactThumbnailWidth = 55;
 let _compactThumbnailHeight = 30;
 let _normalThumbnailWidth = 80;
 let _normalThumbnailHeight = 50;
 let showComments = ref(true);
 let showCommentsPause = ref(true);
+let thumbnailWidth = ref(compactMode ? _compactThumbnailWidth : _normalThumbnailWidth);
+let thumbnailHeight = ref(compactMode ? _compactThumbnailHeight : _normalThumbnailHeight);
 
 export default function useUI() {
 	function setPlayerHeight(h) {
