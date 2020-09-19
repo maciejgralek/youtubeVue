b7cebf40768a992095c783dfba52e764767aef98
ender <ender@localhost>
2020-09-19 14:16:59 +0200

    add scroll directive

--- src/main.js
+++ src/main.js
@@ -5,4 +5,17 @@ import { Dropdown } from 'bootstrap'
 import '@mdi/font/css/materialdesignicons.css'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 
-createApp(App).use(router).mount('#app')
+let app = createApp(App);
+app.use(router).mount('#app');
+
+app.directive('scroll', {
+  mounted: function (el, binding) {
+    let f = function (evt) {
+      if (binding.value(evt, el)) {
+        el.removeEventListener('scroll', f)
+      }
+    }
+    el.addEventListener('scroll', f)
+  }
+})
+
