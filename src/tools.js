b67c0753961be7620697dd1b0cb3294d6ee21708
ender <ender@localhost>
2020-11-06 18:30:10 +0100

    add debounce function

--- src/tools.js
+++ src/tools.js
@@ -16,3 +16,18 @@ export function getRandomInteger(min, max) {
 export function ifMinAddDigit(value) {
   return value < 10 ? '0' + value : value;
 }
+
+export function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
