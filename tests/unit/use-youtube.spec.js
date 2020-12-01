3133cdd09553bd8d8c059dcb38a656516051c7a7
ender <ender@localhost>
2020-12-01 16:03:34 +0100

    add test

---
+++ tests/unit/use-youtube.spec.js
@@ -0,0 +1,33 @@
+import useYoutube from '../../src/use-youtube'
+import axios from 'axios'
+
+let {
+  getPlaylistRemote,
+  addPlaylist,
+} = useYoutube();
+
+jest.mock('axios')
+
+describe('useYoutube', () => {
+  it('test', async () => {
+    axios.get.mockResolvedValue({
+      data: {
+        items: [
+          {
+            id: 1,
+            snippet: {
+              description: 'test',
+              title: 'test',
+            },
+            title: 'test',
+          }
+        ]
+      }
+    })
+
+    let playlist = await addPlaylist('test', false);
+    console.log(playlist)
+  })
+})
+
+
