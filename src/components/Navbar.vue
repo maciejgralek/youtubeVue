6f90ed19dc3c6a77870c01a60ff3be2239340d58
ender <ender@localhost>
2020-12-09 20:42:12 +0100

    remove extra class

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark-l10 py-0">
+  <nav class="navbar navbar-expand-lg navbar-dark py-0">
   <div class="container-fluid">
     <a class="navbar-brand font-weight-bold" href="#">YTPlay</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
