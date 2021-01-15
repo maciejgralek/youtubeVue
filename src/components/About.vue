296fe7b2174065c7104ac4d0bb23d3485a68711d
ender <ender@localhost>
2021-01-15 15:21:50 +0100

    format text

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,26 +1,32 @@
 <template>
-  <div class="row p-3 m-4">
-    <div class="col">
-        <h5>
-          Display and listen to multiple YouTube playlists. It will try to entertain by playing comments.
-        </h5>
-      <hr>
-      <h5><i class="mdi mdi-play me-2"></i>How to add playlist</h5>
-      <p class="mb-0">
+  <div class="about p-5">
+    <h5 class="mb-5">
+      Display and listen to multiple YouTube playlists. It will try to entertain by playing comments.
+    </h5>
+    <div class="mb-5">
+      <h5 class="mb-4">
+        <i class="mdi mdi-play me-2"></i>
+        How to add playlist
+      </h5>
+      <p class="mb-0 ms-5">
         Add comma seperated playlist IDs in URL
       </p>
-      <p class="d-inline-block p-1 m-2">
+      <p class="d-inline-block m-2 ms-5">
         <a href="https://ytplay.netlify.app/playlist/PL6KKV5307aucIh814g2_yPUztUhLGCPbt,PL6KKV5307auf79PLK0fTDnuMfP3sOOchX">
           https://ytplay.netlify.app/playlist/PL6KKV5307aucIh814g2_yPUztUhLGCPbt,PL6KKV5307auf79PLK0fTDnuMfP3sOOchX
         </a>
       </p>
-      <p>
+      <p class="ms-5">
         or click
         <i class="mdi mdi-playlist-music mdi-24px"></i>
       </p>
-      <hr>
-      <h5><i class="mdi mdi-play me-2"></i>Search is not working</h5>
-      <p>
+    </div>
+    <div class="mb-5">
+      <h5 class="mb-4">
+        <i class="mdi mdi-play me-2"></i>
+        Search is not working
+      </h5>
+      <p class="ms-5">
         Search is temporary disabled because of YouTube API quota.
       </p>
     </div>
